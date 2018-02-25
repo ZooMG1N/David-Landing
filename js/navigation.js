609394c052fe7e0cee422d3c3720b5c3dbbc873d
ZooMG1N <zoomg1next@gmail.com>
2018-02-25 15:26:30 +0200

    added anchor links and site navigation

--- js/navigation.js
+++ js/navigation.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('.menu-link').click(function () {
         var elementClick = $(this).attr("href");
         var destination = $(elementClick).offset().top;
-        var navbarHeight = $(".menu").outerHeight();
-        $('html').animate({ scrollTop: destination-navbarHeight}, 1000);
+        $('html').animate({ scrollTop: destination}, 500);
     });
 });
+
