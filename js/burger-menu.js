5e10ecc8a0dab9f667e0d0d1ddf36b6ce8c488a1
ZooMG1N <zoomg1next@gmail.com>
2018-02-26 21:22:11 +0200

    returned a deleted string in js

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -4,13 +4,13 @@ let menu = document.querySelector('.menu');
 burger.addEventListener('click', function () {
     this.classList.toggle('open');
 
+    menu.classList.toggle('open');
     if (menu.classList.contains('open')) {
         menu.style.display = 'flex';
     } else {
         menu.style.display = 'none';
     }
 });
-
 
 if (window.innerWidth > 900) {
     burger.style.display = 'none';
